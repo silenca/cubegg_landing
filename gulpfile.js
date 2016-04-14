bf870086000bde14ebf8e70e46e1f4686a26beb7
Kostya Moiseenko <polocatik@bk.ru>
2016-04-14 11:58:04 +0300

    Add projet to git repository. Add validation for sign up form

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp    = require('gulp');
+	connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  	connect.server({
+  		port: 8000,
+  		livereload: true
+  	});
+});
+
+
+
+gulp.task('default', ['connect']);
